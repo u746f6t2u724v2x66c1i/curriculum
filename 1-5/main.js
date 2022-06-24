5118479171fc5eb9afa8770c36a2ca3bfe6a0c64
chikashishirozu <102590095+chikashishirozu@users.noreply.github.com>
2022-06-24 14:51:44 +0900

    良く見たら違っていたので再提出します。
    宜しくお願いします。

--- 1-5/main.js
+++ 1-5/main.js
@@ -1,9 +1,9 @@
 //①関数名を適切なものに変えてください	
 //②仮引数を適切なものに変えてください
 let fruits = createJuice('みかん');
-    //③「〇〇を受け取りました。ジュースにして返します」と出力
+function createJuice(fruits) {
+	//③「〇〇を受け取りました。ジュースにして返します」と出力
     console.log(fruits + 'を受け取りました。ジュースにして返します');
-function createJuice(fruits) {   
     //④受け取った果物にジュースという文字列を結合して、呼び出し元に返す
     return (fruits + 'ジュース');
 }
