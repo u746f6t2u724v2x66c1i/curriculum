fa0a92f51db303baa9b5c209ba55c38ad11cb6e6
chikashishirozu <102590095+chikashishirozu@users.noreply.github.com>
2022-08-13 13:48:11 +0900

    上手く出来ませんでした。
    どこが間違っていますか？

---
+++ 1-7/main.js
@@ -0,0 +1,24 @@
+//Taiyakiクラスを作成
+class Taiyaki {
+
+	//コンストラクタ
+	constructor(nakami){
+		this.nakami = nakami;
+	}
+	
+	//食べるメソッド（関数）作成
+	eat()　{
+		console.log('中身は｛this.nakami｝です');
+	}
+}
+//「あんこ」オブジェクト作成
+let anko = new Taiyaki('あんこ');
+anko.eat();
+
+//「クリーム」オブジェクト作成
+let cream = new Taiyaki('クリーム');
+cream.eat();
+
+//「チーズ」オブジェクト作成
+let cheese = new Taiyaki('チーズ');
+cheese.eat();
