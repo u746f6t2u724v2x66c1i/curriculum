5073dc5db95ac29436f9f69b369e82a075bc7a0a
chikashishirozu <102590095+chikashishirozu@users.noreply.github.com>
2022-08-13 18:42:29 +0900

    上手く動きません。
    上手く動きません。

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-var price = 100;
-console.log(price * 2);
+//Taiyakiクラスを作成
+class Taiyaki {
 
-price = 500;
-console.log(price * 2);
+	//コンストラクタ
+	constructor(nakami){
+		this.nakami = nakami;
+	}
+	
+	//食べるメソッド（関数）作成
+	eat()　{
+		console.log('中身は${this.nakami}です');
+	}
+}
+//「あんこ」オブジェクト作成
+let anko = new Taiyaki('あんこ');
+anko.eat();
+
+//「クリーム」オブジェクト作成
+let cream = new Taiyaki('クリーム');
+cream.eat();
+
+//「チーズ」オブジェクト作成
+let cheese = new Taiyaki('チーズ');
+cheese.eat();
