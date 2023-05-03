4d3b01ea263c67c4c612b52175afb307c9b0f301
chikashishirozu <102590095+chikashishirozu@users.noreply.github.com>
2023-05-03 17:23:01 +0900

    Create bubbleSort.cpp
    バブルソートアルゴリズム
    バブル ソートは、リストを繰り返し処理し、隣接する要素を比較し、順序が間違っている場合は入れ替える単純な並べ替えアルゴリズムです。 リストがソートされるまで、リストのパスが繰り返されます。 C++ での実装例を次に示します。

---
+++ bubbleSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n-1; i++) {
+        swapped = false;
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
