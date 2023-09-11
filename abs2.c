a58e217c46bf253bd8d2a706a73aefe028dd8d23
chikashishirozu <102590095+chikashishirozu@users.noreply.github.com>
2023-09-11 14:07:56 +0900

    update?
    [hiroppy123@fedora ~]$ gcc -o abs2 abs2.c
    [hiroppy123@fedora ~]$ ./abs2
    468
    3467
    3935
    2999

---
+++ abs2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h> // Include the stdlib.h header for the abs() function
+
+void update(int *a, int *b) {
+    // Complete this function
+}
+
+int main() {
+    int a, b;
+    // int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    // update(pa, pb);
+    printf("%d\n%d\n", a + b, abs(a - b)); // Use abs(a - b) to calculate the absolute difference
+
+    return 0;
+}
+
