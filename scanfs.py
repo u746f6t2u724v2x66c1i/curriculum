abce97adf7bcaf53e01ef59fbcfe5ce8d486b5af
chikashishirozu <102590095+chikashishirozu@users.noreply.github.com>
2023-09-12 15:01:02 +0900

    input_string = input()
    [hiroppy123@fedora ~]$ python3 scanfs.py
    Welcometonewworld!
    Hello, World.
    Welcometonewworld!

---
+++ scanfs.py
@@ -0,0 +1,9 @@
+# Read a line of input from the user and store it in a variable
+input_string = input()
+
+# Print a string literal saying "Hello, World." to the console
+print("Hello, World.")
+
+# Print the contents of input_string to the console
+print(input_string)
+
