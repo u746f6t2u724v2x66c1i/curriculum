70aebb30ab05db8ce8467456f95713f4f192da4f
chikashishirozu <102590095+chikashishirozu@users.noreply.github.com>
2023-09-15 18:01:01 +0900

    equals
    [hiroppy123@fedora ~]$ javac equals.java
    [hiroppy123@fedora ~]$ java equals
    paiza
    YES
    [hiroppy123@fedora ~]$ java equals
    today
    NO

---
+++ equals.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class equals {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String tokenA = sc.next();
+
+        // Use equals() to compare strings
+        if (tokenA.equals("paiza")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
