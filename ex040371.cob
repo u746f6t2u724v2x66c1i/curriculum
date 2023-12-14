9949f3b66a35d482e76c2a20a924fc1883562b35
chikashishirozu <102590095+chikashishirozu@users.noreply.github.com>
2023-12-14 11:31:33 +0900

    平均点と合計点
    hiroppy123@fedora:~$ cobc -x -v ex040371.cob
    cobc (GnuCOBOL) 3.2.0
    Built     Oct 19 2023 00:00:00	Packaged  Jul 28 2023 17:02:56 UTC
    C version "13.2.1 20230918 (Red Hat 13.2.1-3)"
    loading standard configuration file 'default.conf'
    command line:	cobc -x -v ex040371.cob 
    ex040371.cob:1: note: free format detected
        1 > identification     division.
        2 | program-id.        ex04037.
        3 | data               division.
    preprocessing:	ex040371.cob -> /tmp/cob95109_0.cob
    return status:	0
    parsing:	/tmp/cob95109_0.cob (ex040371.cob)
    return status:	0
    translating:	/tmp/cob95109_0.cob -> /tmp/cob95109_0.c (ex040371.cob)
    executing:	gcc -c -fexceptions -grecord-gcc-switches -pipe -Wall
    		-Werror=format-security
    		-Wp,-U_FORTIFY_SOURCE,-D_FORTIFY_SOURCE=3
    		-Wp,-D_GLIBCXX_ASSERTIONS
    		-specs=/usr/lib/rpm/redhat/redhat-hardened-cc1
    		-fstack-protector-strong
    		-specs=/usr/lib/rpm/redhat/redhat-annobin-cc1 -m64
    		-mtune=generic -fstack-clash-protection -fcf-protection
    		-fno-omit-frame-pointer -mno-omit-leaf-frame-pointer
    		-fstack-protector-strong -pipe -U_FORTIFY_SOURCE
    		-D_FORTIFY_SOURCE=2 -I/usr/include -Wno-unused -fsigned-char
    		-Wno-pointer-sign -O -o "/tmp/cob95109_0.o"
    		"/tmp/cob95109_0.c"
    return status:	0
    executing:	gcc -Wl,--export-dynamic -o "ex040371" "/tmp/cob95109_0.o"
    		-Wl,-z,relro -Wl,--as-needed -Wl,-z,now
    		-specs=/usr/lib/rpm/redhat/redhat-hardened-ld
    		-specs=/usr/lib/rpm/redhat/redhat-annobin-cc1
    		-Wl,--build-id=sha1 -fstack-protector-strong -L/usr/lib64
    		-lcob
    return status:	0
    hiroppy123@fedora:~$ ./ex040371
    点数を入力してください。
    87
    平均点数＝00000087
    合計点数＝00000087
    点数を入力してください。
    97
    平均点数＝00000092
    合計点数＝00000184
    点数を入力してください。
    56
    平均点数＝00000080
    合計点数＝00000240
    点数を入力してください。
    85
    平均点数＝00000081
    合計点数＝00000325
    点数を入力してください。
    64
    平均点数＝00000077
    合計点数＝00000389
    点数を入力してください。
    86
    平均点数＝00000079
    合計点数＝00000475
    点数を入力してください。
    95
    平均点数＝00000081
    合計点数＝00000570
    点数を入力してください。
    67
    平均点数＝00000079
    合計点数＝00000637
    点数を入力してください。
    88
    平均点数＝00000080
    合計点数＝00000725
    点数を入力してください。
    65
    平均点数＝00000079
    合計点数＝00000790
    点数を入力してください。
    85
    平均点数＝00000079
    合計点数＝00000875
    点数を入力してください。
    97
    平均点数＝00000081
    合計点数＝00000972
    点数を入力してください。
    68
    平均点数＝00000080
    合計点数＝00001040
    hiroppy123@fedora:~$

---
+++ ex040371.cob
@@ -0,0 +1,22 @@
+identification     division.
+program-id.        ex04037.
+data               division.
+working-storage    section.
+01  DATA1          pic 9(3).
+01  COUNTER1       pic 9(8) value zero.
+01  COUNTER2       pic 9(8) value zero.
+01  DATA2          PIC 9(8).
+01  AVERAGE        PIC 9(8).
+procedure          division.
+RTN-START.
+    perform  until  COUNTER2  >  999
+        display  "点数を入力してください。"
+        accept  DATA1
+        add  1  to  COUNTER1
+        add  DATA1  to  COUNTER2
+        compute  DATA2  =  COUNTER2  /  COUNTER1  
+        display  "平均点数＝"  DATA2
+        display  "合計点数＝"  COUNTER2
+    end-perform.
+    stop  run.
+    
